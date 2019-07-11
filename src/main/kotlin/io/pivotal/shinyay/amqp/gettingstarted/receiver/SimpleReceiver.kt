68e48ecf16b1eaf33b487db7e74f46d2d48ba5fd
shinyay <shinya.com@gmail.com>
2019-07-11 23:56:08 +0900

    Add message receiver modile

---
+++ src/main/kotlin/io/pivotal/shinyay/amqp/gettingstarted/receiver/SimpleReceiver.kt
@@ -0,0 +1,29 @@
+package io.pivotal.shinyay.amqp.gettingstarted.receiver
+
+import org.springframework.amqp.core.Binding
+import org.springframework.amqp.core.BindingBuilder
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.core.TopicExchange
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+
+@Component
+class SimpleReceiver : CommandLineRunner {
+
+    val queueNameHello = "queue-hello"
+    val topicNameHello = "topic-hello"
+
+    @Bean
+    fun queue(): Queue = Queue(queueNameHello, false)
+
+    @Bean
+    fun exchange(): TopicExchange = TopicExchange(topicNameHello)
+
+    @Bean
+    fun binding(queue: Queue, topicExchange: TopicExchange) : Binding
+            = BindingBuilder.bind(queue).to(topicExchange).with(queueNameHello)
+    
+    override fun run(vararg args: String?) {
+    }
+}
