4d7398332a2d39db0f7f42a97014e9a7000637eb
shinyay <shinya.com@gmail.com>
2019-07-12 00:19:18 +0900

    Add message listner for asynchrounous messaging

--- src/main/kotlin/io/pivotal/shinyay/amqp/gettingstarted/receiver/SimpleReceiver.kt
+++ src/main/kotlin/io/pivotal/shinyay/amqp/gettingstarted/receiver/SimpleReceiver.kt
@@ -1,9 +1,13 @@
 package io.pivotal.shinyay.amqp.gettingstarted.receiver
 
+import io.pivotal.shinyay.amqp.gettingstarted.handler.SimpleHandler
 import org.springframework.amqp.core.Binding
 import org.springframework.amqp.core.BindingBuilder
 import org.springframework.amqp.core.Queue
 import org.springframework.amqp.core.TopicExchange
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -25,7 +29,18 @@ class SimpleReceiver : CommandLineRunner {
             = BindingBuilder.bind(queue).to(topicExchange).with(queueNameHello)
 
     @Bean
+    fun listenerAdapter(handler: SimpleHandler): MessageListenerAdapter
+            = MessageListenerAdapter(handler, "handleMessage")
 
+    @Bean
+    fun container(connectionFactory: ConnectionFactory,
+                  messageListenerAdapter: MessageListenerAdapter): SimpleMessageListenerContainer {
+        val simpleMessageListenerContainer = SimpleMessageListenerContainer()
+        simpleMessageListenerContainer.connectionFactory = connectionFactory
+        simpleMessageListenerContainer.setQueueNames(queueNameHello)
+        simpleMessageListenerContainer.setMessageListener(messageListenerAdapter)
+        return simpleMessageListenerContainer
+    }
 
     override fun run(vararg args: String?) {
     }
