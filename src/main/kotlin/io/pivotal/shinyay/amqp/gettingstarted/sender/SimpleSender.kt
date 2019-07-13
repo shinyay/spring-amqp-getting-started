29195a96510482bbb1a7e930d9fb33a173e9b735
shinyay <shinya.com@gmail.com>
2019-07-13 18:23:11 +0900

    Add sending message with Scheduled annotation

--- src/main/kotlin/io/pivotal/shinyay/amqp/gettingstarted/sender/SimpleSender.kt
+++ src/main/kotlin/io/pivotal/shinyay/amqp/gettingstarted/sender/SimpleSender.kt
@@ -5,8 +5,12 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import java.util.*
 
 @Configuration
+@EnableScheduling
 class SimpleSender(val rabbitTemplate: RabbitTemplate) : CommandLineRunner {
 
     val queueNameHello = "queue-hello"
@@ -15,8 +19,13 @@ class SimpleSender(val rabbitTemplate: RabbitTemplate) : CommandLineRunner {
     fun queueForHelloToSend(): Queue = Queue(queueNameHello, false)
 
     override fun run(vararg args: String?) {
-        rabbitTemplate.convertAndSend(queueNameHello, ">> Hello, RabbitMQ! from SimpleSender")
+        val sendTime = Date().toString()
+        rabbitTemplate.convertAndSend(queueNameHello, ">> Hello, RabbitMQ! by CommandLineRunner at $sendTime")
     }
 
-
+    @Scheduled(fixedDelay = 5000)
+    fun sendMessage() {
+        val sendTime = Date().toString()
+        rabbitTemplate.convertAndSend(queueNameHello, ">> Hello, RabbitMQ! by EnabledScheduling at $sendTime")
+    }
 }
